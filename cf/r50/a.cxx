82327edf833193f87be926849c0b04127c07dce2
Osalotioman <osalotioman@gmail.com>
2025-12-29 03:10:19 +0100

    50A

---
+++ cf/r50/a.cxx
@@ -0,0 +1,14 @@
+/*
+    author: Osalotioman
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    cout << (n*m)/2;
+    return 0;
+}
