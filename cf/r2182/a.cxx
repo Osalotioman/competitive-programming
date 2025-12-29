52247c218a60e528d56e8f6f17708d2038836ac2
Osalotioman <osalotioman@gmail.com>
2025-12-29 19:04:28 +0100

    incomplete - 2182A

---
+++ cf/r2182/a.cxx
@@ -0,0 +1,16 @@
+/*
+    author: Osalotioman
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc; 
+    cin >> tc;
+    while(tc--){
+
+    }
+    return 0;
+}
