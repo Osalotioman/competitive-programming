81c83e5147408f9390f478e376b248989358f1aa
Osalotioman <osalotioman@gmail.com>
2026-01-31 21:11:02 +0100

    feat: r1760a

---
+++ cf/r1760/a.cxx
@@ -0,0 +1,21 @@
+/*
+    author: Osalotioman
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc; 
+    cin >> tc;
+    while(tc--){
+        vector<int> a(3);
+        for(int i = 0; i<3; ++i){
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        cout << a[1] << '\n';
+    }
+    return 0;
+}
